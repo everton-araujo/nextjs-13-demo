d6d70672318fb9707b53ad6267195bfb76268b83
everton-araujo <everton.aaraujo@outlook.com>
2023-01-12 08:29:37 -0300

    Header link added

--- app/Header.tsx
+++ app/Header.tsx
@@ -4,6 +4,7 @@ export default function Header() {
     return (
         <div className='p-5 bg-blue-500'>
             <Link href='/' className="px-2 py-1 bg-white text-blue-500 rounded-lg">Home</Link>
+            <Link href='/todos' className="ml-4 px-2 py-1 bg-white text-blue-500 rounded-lg">Todos</Link>
         </div>
     )
 }
