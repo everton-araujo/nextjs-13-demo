9168f898fc5d36ae093bf24ef7a8a07069fd6c1b
everton-araujo <everton.aaraujo@outlook.com>
2023-01-12 08:24:00 -0300

    First commit

---
+++ app/Header.tsx
@@ -0,0 +1,9 @@
+import Link from "next/link";
+
+export default function Header() {
+    return (
+        <div className='p-5 bg-blue-500'>
+            <Link href='/' className="px-2 py-1 bg-white text-blue-500 rounded-lg">Home</Link>
+        </div>
+    )
+}
